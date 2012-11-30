46d61cdf44ce2b7eed3ff4bf8c804c710287e4fb
thisIStheRUKUS <connor.bode@gmail.com>
2012-11-29 19:56:56 -0800

    Finished method for adding new events to calendars. Added comments for functions specifying parameters. Added links to relevant resources

--- src/timeslot.js
+++ src/timeslot.js
@@ -8,7 +8,13 @@
 
 
 /**
- * Creates a timeslot object given startTime, endTime, name, location.
+	Creates a timeslot object given startTime, endTime, name, location.
+	
+	@param startTime A datetime representing the start of the timeslot
+	@param endTime A datetime representing the end of a timeslot
+	@param name The name of the event (timeslot label)
+	@param location The location where the event will occur
+	@return A timeslot object representing an event
  */
 function timeslot( startTime, endTime, name, location ) {
 	x = {
@@ -23,15 +29,18 @@ function timeslot( startTime, endTime, name, location ) {
 
 
 /**
-	Adds a new calendar.
+	Creates a new calendar in Google Calendar and populates the calendar with events.
 	
-	name is the name of the calendar
-	remindMethod is the reminder method for the calendar, options are: "email", "sms", "popup" -- see https://developers.google.com/google-apps/calendar/v3/reference/calendarList/insert
-	remindMinutes is the number of minutes before the start of the event that the reminder should be triggered.
-	remindEmail is the email which reminders will be sent to if remindMethod is set to "email"
-	timeslots is an array of timeslot objects.
+	@param name The name of the calendar
+	@param remindMethod The reminder method for the calendar, options are: "email", "sms", "popup" -- see https://developers.google.com/google-apps/calendar/v3/reference/calendarList/insert
+	@param remindMinutes The number of minutes before the start of the event that the reminder should be triggered.
+	@param remindEmail The email which reminders will be sent to if remindMethod is set to "email"
+	@param timeslots An array of timeslot objects
+	@param endOfSemester A datetime representing the end of classes for the semester.  Used to mark the end of event recurrence
+
+	Specifications for datetime can be found here: http://tools.ietf.org/html/rfc3339#section-5
 **/
-function addNewCalendar( name, timeslots, remindMethod, remindMinutes, remindEmail ) {
+function addNewCalendar( name, timeslots, remindMethod, remindMinutes, remindEmail, endOfSemester ) {
 
 	// Create a new calendar request
 	var request = gapi.client.request({
@@ -48,6 +57,7 @@ function addNewCalendar( name, timeslots, remindMethod, remindMinutes, remindEma
 	
 		if( response.status == 200 ) {
 			
+			// Grab the id for the newly created valendar
 			var id = response.id;
 			
 			console.log( "Calendar '" + name + "' added with id " + id );
@@ -85,17 +95,37 @@ function addNewCalendar( name, timeslots, remindMethod, remindMinutes, remindEma
 									'minutes': remindMinutes
 								}
 							]
-						}
+						},
+						
+						// Recurrence rules according to iCalendar format
+						// See http://googleappsdeveloper.blogspot.ca/2011/12/calendar-v3-best-practices-recurring.html for explanation
+						// or http://tools.ietf.org/html/rfc5545#section-3.3.10 for RRULE specifications
+						'recurrence': [
+							"RRULE:FREQ=WEEKLY;UNTIL=" + endOfSemester
+						]
 					}
 				});
 				
 				// Perform the request
 				timeslotRequest.execute( function( response2 ) {
 				
+					if( response2.status == 200 ) {
+					
+						// Successfully added the event
+						console.log( timeslot.name + " event added successfully." );
+					} else {
+						// Couldn't add event
+						console.log( "Error adding event " + timeslot.name + "." );
+						console.log( "HTTP Response Status: " + response2.status );
+						console.log( "HTTP Response: " + response2 );
+					}
+				
 				});
 			}
 		} else {
-			// Couldn't contact....
+			// Couldn't add calendar
+			console.log( "Error creating calendar." );
+			console.log( "HTTP Response Status: " + response.status );
 		};
 	});
 }
